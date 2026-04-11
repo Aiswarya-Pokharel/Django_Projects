18d85bc46a7a758714e2bbef6d8cb2d1bd0c93dc
Aiswarya Pokharel <pokharelaishwarya22@gmail.com>
2026-04-11 22:01:33 +0545

    connection with database

--- students/frontend/views.py
+++ students/frontend/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from student_registration.models import Student
 
-# Create your views here.
+def student_form(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        age = request.POST['age']
+        roll = request.POST['roll']
+        address = request.POST['address']
+        Student.objects.create(name=name, age=age, roll=roll, address=address)
+        return redirect('student_form')
+    
+    students = Student.objects.all()
+    return render(request, 'student.html', {'students': students})
--- students/student_registration/models.py
+++ students/student_registration/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    sn = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, default='')
+    age = models.IntegerField(default=0)
+    roll = models.IntegerField(default=0)
+    address = models.CharField(max_length=200, default='')
+
+    def __str__(self):
+        return self.name
