d4b5e14a68fd0e19e01f090d9da58947a5ef2df9
Aiswarya Pokharel <pokharelaishwarya22@gmail.com>
2026-04-12 15:21:19 +0545

    removed appointment_project folder

--- crm_project/crm_app/apps.py
+++ crm_project/crm_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FrontendConfig(AppConfig):
+class CrmAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'frontend'
+    name = 'crm_app'
